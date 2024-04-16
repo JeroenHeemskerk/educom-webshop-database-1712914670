88c886b151c707d7aeeb4b748e29814482d4a77f
uknowwho <florianvandersteen@gmail.com>
2024-04-16 15:25:30 +0200

    02C: opdr 3.2, issue 5; complete werkende shopping cart

--- cart.php
+++ cart.php
@@ -1,17 +1,23 @@
 <?php 
 
 function showCartContent () {
+    echo '<h2>Winkelmandje</h2>';
+
     include_once('communication.php');
     $products = getCartProducts();
 
     $cartCounts = getCartCounts();
+    $total = 0.0;
     foreach($products as $product) {
-        echo '<div class="cart-list">';
+        $total += $cartCounts[$product["id"]] * $product["price"];
+        echo '<a class="cart-list" href="index.php?page=shop&product=' . $product["id"] . '">';
+        echo '<div>';
         echo '<img src=Images/' . $product["fname"] . '>';
         echo '<p>Aantal: ' . $cartCounts[$product["id"]] . '<br>';
-        echo 'Prijs: ' . $product["price"] . '<br>';
+        echo 'Prijs: &euro;' . $product["price"] . ',-<br>';
         echo '</p>';
-        echo '</div>';
+        echo '</div></a>';
     }
+    echo '<p id="total-cart">Totaal: &euro;' . $total . ',-</p>';
 }
 
